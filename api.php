ae798fe540c40e462c80d91038affbffc6861951
Augusto Martinez <acm_asd@hotmail.com>
2020-08-08 14:22:13 -0600

    Update api.php

--- api.php
+++ api.php
@@ -3,6 +3,9 @@
 require_once ("getjson.php");
 
 class Api {
+  
+  private $user;
+  private $pass;
     
   public function __construct() {
     header ("Access-Control-Allow-Origin: *");
@@ -11,9 +14,46 @@ class Api {
     header ("Access-Control-Max-Age: 3600");
     header ("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     
+    $valid = array(
+     "guest" => "mypass",
+    );
+    
+    $valid_users = array_keys($valid);
+    
+    if (!isset($_SERVER["PHP_AUTH_PW"]) || !isset($_SERVER["PHP_AUTH_PW"])) {
+      http_response_code(400);
+      header ("WWW-Authenticate: Basic realm=\"My Realm\"");
+      header ("HTTP/1.0 401 Unauthorized");
+      
+      $mess = array(
+        "message" => "Not authorized call",
+      );
+      
+      echo json_encode($mess);      
+      exit();
+    }
+    
+    $user = $_SERVER["PHP_AUTH_USER"];
+    $pass = $_SERVER["PHP_AUTH_PW"];
+    
+    $validated = (in_array($user, $valid_users)) && ($pass == $valid[$user]);
+
+    if (!$validated) {
+      http_response_code(400);
+      header ("WWW-Authenticate: Basic realm=\"My Realm\"");
+      header ("HTTP/1.0 401 Unauthorized");
+      
+      $mess = array(
+        "message" => "Not authorized call",
+      );
+      
+      echo json_encode($mess);      
+      exit();
+    }
+        
     $country = isset($_GET["country"]) ? $_GET["country"] : null;    
     $country = $this->sanitize_str($country);
-
+        
     $data = new getJSON($country);
 
     if ($data->error) {
@@ -38,6 +78,10 @@ class Api {
     $str = htmlspecialchars($str);
     
     return $str;
+  }
+  
+  public function auth($token) {
+    
   }
 }
 
